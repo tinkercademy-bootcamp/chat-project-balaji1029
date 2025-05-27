e0855508e80907e5d43643c17bf7d447008653da
Balaji <balajikaredla4@gmail.com>
2025-05-27 19:50:39 +0000

    create server address method

--- exercise-5/src/client-main.cc
+++ exercise-5/src/client-main.cc
@@ -1,4 +1,18 @@
+#include "client/chat-client.h"
+#include "../utils.h"
 
+std::string read_args(int argc, char *argv[]) {
+  using namespace tt::chat;
+  std::string message = "Hello from client";
+  if (argc == 1) {
+    std::cout << "Usage: " << argv[0] << " <message>\n";
+    exit(EXIT_FAILURE);
+  }
+  if (argc > 1) {
+    message = argv[1];
+  }
+  return message;
+}
 
 int main(int argc, char *argv[]) {
   const int kPort = 8080;
--- exercise-5/src/client/chat-client.h
+++ exercise-5/src/client/chat-client.h
@@ -15,7 +15,36 @@
 
 namespace tt::chat::client {
 
-  
+  class Client {
+    sockaddr_in create_server_address(const std::string &server_ip, int port);
+  };
+
+
+void connect_to_server(int sock, sockaddr_in &server_address) {
+  using namespace tt::chat;
+  auto err_code =
+      connect(sock, (sockaddr *)&server_address, sizeof(server_address));
+  check_error(err_code < 0, "Connection Failed.\n");
+}
+
+void send_and_receive_message(int sock, const std::string &message) {
+  using namespace tt::chat;
+  const int kBufferSize = 1024;
+  char recv_buffer[kBufferSize] = {0};
+
+  // Send the message to the server
+  send(sock, message.c_str(), message.size(), 0);
+  std::cout << "Sent: " << message << "\n";
+
+  // Receive response from the server
+  ssize_t read_size = read(sock, recv_buffer, kBufferSize);
+  check_error(read_size < 0, "Read error.\n");
+  if (read_size > 0) {
+    std::cout << "Received: " << recv_buffer << "\n";
+  } else if (read_size == 0) {
+    std::cout << "Server closed connection.\n";
+  }
+}
 
 } // namespace tt::chat::client
 
