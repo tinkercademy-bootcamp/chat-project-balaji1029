d5aae6b3c0c94332d3dd02fce58153a3b0772076
Balaji <balajikaredla4@gmail.com>
2025-05-27 18:52:49 +0000

    handle accept

--- exercise-5/src/server/chat-server.h
+++ exercise-5/src/server/chat-server.h
@@ -18,28 +18,10 @@ namespace tt::chat::server {
     int create_server_socket();
     void bind_address_to_socket(int sock, sockaddr_in &address);
     void listen_on_socket(int sock);
+    void handle_accept(int sock);
+
   };
-
-  void handle_accept(int sock) {
-    namespace ttc = tt::chat;
-    const int kBufferSize = 1024;
-    char buffer[kBufferSize] = {0};
-    ssize_t read_size = read(sock, buffer, kBufferSize);
-
-    ttc::check_error(read_size < 0,
-                    "Read error on client socket " + std::to_string(sock));
-    if (read_size > 0) {
-      std::cout << "Received:" << buffer << "\n";
-      send(sock, buffer, read_size, 0);
-      std::cout << "Echo message sent\n";
-    } else if (read_size == 0) {
-      std::cout << "Client disconnected.\n";
-    } else {
-      std::cerr << "Read error on client socket " << sock << "\n";
-    }
-    close(sock);
-  }
-
+  
   sockaddr_in create_server_address(int port) {
     namespace ttn = tt::chat::net;
     sockaddr_in address = ttn::create_address(port);
