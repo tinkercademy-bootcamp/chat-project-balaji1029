3ed21200d79f32968e70ad84ce8c18915ecea95c
Balaji <balajikaredla4@gmail.com>
2025-05-27 18:50:15 +0000

    make bind_address_to_socket

--- exercise-5/src/server/chat-server.h
+++ exercise-5/src/server/chat-server.h
@@ -16,15 +16,9 @@ namespace tt::chat::server {
 
     void set_socket_options(int sock, int opt);
     int create_server_socket();
+    void bind_address_to_socket(int sock, sockaddr_in &address);
 
   };
-
-
-  void bind_address_to_socket(int sock, sockaddr_in &address) {
-    namespace ttc = tt::chat;
-    auto err_code = bind(sock, (sockaddr *)&address, sizeof(address));
-    ttc::check_error(err_code < 0, "bind failed\n");
-  }
 
   void listen_on_socket(int sock) {
     namespace ttc = tt::chat;
